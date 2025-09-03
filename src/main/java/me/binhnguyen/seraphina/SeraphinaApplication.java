2a4667e0f27174dda4cd8c01275dc61a7657a5f2
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-09-03 21:54:39 +0700

    first init

---
+++ src/main/java/me/binhnguyen/seraphina/SeraphinaApplication.java
@@ -0,0 +1,13 @@
+package me.binhnguyen.seraphina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeraphinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeraphinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/binhnguyen/seraphina/SeraphinaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.binhnguyen.seraphina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeraphinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
