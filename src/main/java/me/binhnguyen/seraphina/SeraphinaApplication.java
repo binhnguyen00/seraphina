1920a117c25f12ae8699cff0109348336f9314db
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-09-03 22:09:30 +0700

    update code

--- src/main/java/me/binhnguyen/seraphina/SeraphinaApplication.java
+++ src/main/java/me/binhnguyen/seraphina/SeraphinaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeraphinaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeraphinaApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SeraphinaApplication.class, args);
+  }
 
 }
--- src/test/java/me/binhnguyen/seraphina/SeraphinaApplicationTests.java
+++ src/test/java/me/binhnguyen/seraphina/SeraphinaApplicationTests.java
@@ -1,13 +1,14 @@
 package me.binhnguyen.seraphina;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class SeraphinaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+  }
 }
