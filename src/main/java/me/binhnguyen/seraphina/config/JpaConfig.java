499b7c700d4591fac0fae787970e5650d1c39486
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-09-08 14:58:39 +0700

    save createdTime and modifiedTime on persist

---
+++ src/main/java/me/binhnguyen/seraphina/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package me.binhnguyen.seraphina.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
